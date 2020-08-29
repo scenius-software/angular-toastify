27ab736483bb444d15bb1dda3d473f4cd81460bf
Nick <nick.heskes@scenius.nl>
2020-08-29 10:02:08 +0200

    Bump versions

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -4,7 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AngularToastifyModule, ToastService } from 'projects/angular-toastify/src/public-api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule, MatCardModule, MatInputModule, MatSelectModule, MatFormFieldModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
