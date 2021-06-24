816c0c870f14666445b0add3c5b752cbdc316fe7
Willem <willem.sporen@scenius.nl>
2021-06-24 19:58:41 +0200

    step 2

--- projects/angular-toastify/src/lib/toastify-toast-container/toastify-toast-container.component.spec.ts
+++ projects/angular-toastify/src/lib/toastify-toast-container/toastify-toast-container.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ToastifyToastContainerComponent } from './toastify-toast-container.component';
 
@@ -6,7 +6,7 @@ describe('ToastifyToastContainerComponent', () => {
   let component: ToastifyToastContainerComponent;
   let fixture: ComponentFixture<ToastifyToastContainerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ToastifyToastContainerComponent ]
     })
